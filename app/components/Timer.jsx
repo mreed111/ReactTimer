2d2c1f223c24f345a50c7a5851359166bc6cff27
Mike Reed <mike.reed.vallejo@gmail.com>
2016-08-28 12:01:18 -0700

    add Timer functionality and test file

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -3,13 +3,70 @@ var Clock = require('Clock');
 var CountdownForm = require('CountdownForm');
 
 var Clock = require('./Clock');
+var Controls = require('Controls');
 
 var Timer = React.createClass({
+  getInitialState: function () {
+    return {
+      count: 0,
+      countdownStatus: 'paused'
+    };
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer)
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count + 1;
+      this.setState({
+        count: newCount < 3600 ? newCount : 0
+      });
+
+      if (newCount === 0) {
+        this.setState({countdownStatus: 'paused'});
+      }
+    }, 1000);
+  },
+  handleStatusChange: function (newStatus) {
+    if (newStatus === 'stopped') {
+      this.setState({
+        count: 0,
+        countdownStatus: 'paused'
+      });
+    } else {
+      this.setState({
+        countdownStatus: newStatus
+      });
+    }
+  },
   render: function () {
+    var {count, countdownStatus} = this.state;
+    var renderControlArea = () => {
+      if (countdownStatus === 'stopped') {
+        this.setState({countdownStatus: 'paused'});
+
+      }
+    };
+
     return (
       <div>
-        <Clock totalSeconds={222}/>
+        <h1 className="page-title">Timer</h1>
+          <Clock totalSeconds={count}/>
 
+          <Controls countdownStatus={countdownStatus}
+            onStatusChange={this.handleStatusChange}/>
       </div>
     );
   }
